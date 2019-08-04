4f05c018ff441a9d01238d2ee32bc738de2a68df
Kevin Schlosser <drschlosser@hotmail.com>
2019-08-04 03:40:32 -0600

    fixes process name

--- pyWinVirtualDesktop/winuser.py
+++ pyWinVirtualDesktop/winuser.py
@@ -135,7 +135,7 @@ def GetProcessName(hwnd):
     res = ''
 
     for i in range(260):
-        if lpExeName[i] == '\x00':
+        if ord(lpExeName[i]) == 0x0:
             continue
         res += lpExeName[i]
 
